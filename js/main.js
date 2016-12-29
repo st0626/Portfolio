a5c207873fb2d1d292d16509612a2d164c1371b5
Byron Tollison <st06266@gmail.com>
2016-12-29 14:13:01 -0600

    started the query "in-view"

--- js/main.js
+++ js/main.js
@@ -1,1 +1,26 @@
-$("#twitter-logo").hover().animate("easing");
+var $animation_elements('.animation-element');
+var $window = $(window);
+
+$window.on('scroll resize' , check_if_in_view);
+$window.trigger('scroll');
+
+function check_if_in_view(){
+    var window_height = $window.height();
+    var window_top_position = $window.scrollTop();
+    var window_bottom_position = (window_top_position + window_height);
+
+    $.each($animation_elements, function(){
+        var $element = $(this);
+        var element_height = $element.outerHeight();
+        var element_top_position = $element.offset().top;
+        var element_bottom_position = (element_top_position + element_height)
+
+        //checking to see if element is in the view port
+        if((element_bottom_position >= window_top_position) && (element_top_position <= window_bottom_position)) {
+            $element.addClass('in-view');
+        } else {
+            $element.removeClass('in-view');
+        }    
+    });
+}
+
