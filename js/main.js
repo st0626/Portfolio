c4f45bf419df406582724b503be968b36f94eed5
byron <st06266@gmail.com>
2016-12-30 21:51:02 -0600

    added auto collaps navbar when link click

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
 $('body').scrollspy({ target: '#mithril-header'});
 $('body').scrollspy({ target: '#contact-header'});
 $('body').scrollspy({ target: '#portfolio-header'});
+
+
+// automatically collapse navbutton on mobile
+
+$('.nav a').on('click', function (){
+    $('.navbar-toggler').click();
+});
